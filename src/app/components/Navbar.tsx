93a30f7a27b6bc09e48b74fcc50a3dbd1f859904
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2024-03-13 08:37:03 +0530

    checkpoint navbar modified

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,25 @@
+import { getServerSession } from "next-auth"
 import Link from "next/link"
+import { options } from "../api/auth/[...nextauth]/options"
+import { redirect } from "next/navigation"
 
-export default function Navbar() {
+
+export default async function Navbar() {
+    const session = await getServerSession(options)
+
     return (
         <nav className="bg-blue-800 p-4">
             <ul className="flex justify-evenly text-2xl font-bold">
                 <li><Link href="/">Home</Link></li>
-                <li><Link href="/signup">Sign Up</Link></li>
-                <li><Link href="/api/auth/signin">Sign In</Link></li>
-                <li><Link href="/api/auth/signout">Sign Out</Link></li>
+                {!session && (
+                    <li><Link href="/signup">Sign Up</Link></li>
+                )}
+                {!session && (
+                    <li><Link href="/api/auth/signin">Sign In</Link></li>
+                )}
+                {session && (
+                    <li><Link href="/api/auth/signout">Sign Out</Link></li>
+                )}
                 <li><Link href="/server">Server</Link></li>
                 <li><Link href="/client">Client</Link></li>
                 <li><Link href="/extra">Extra</Link></li>
